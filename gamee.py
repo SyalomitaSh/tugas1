c1fa9b2d6e2b8bde124b0c043a1e6642878f96bc
syalom <syalomitasharlen1790@gmail.com>
2023-11-04 09:29:38 +0700

    warna

--- gamee.py
+++ gamee.py
@@ -4,12 +4,15 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((450,500))
 clock = pygame.time.Clock()
+test_surface = pygame.Surface((100,200))
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()        
+            sys.exit()
+    screen.fill((190,210,234))
+    screen.blit(test_surface,(200,250))        
     pygame.display.update()
     clock.tick(60)
