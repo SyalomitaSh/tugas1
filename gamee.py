8fe27ada65e859f708cea698ad8cdb4d4fc8d73e
syalom <syalomitasharlen1790@gmail.com>
2023-11-04 08:58:53 +0700

    screen

--- gamee.py
+++ gamee.py
@@ -1,1 +1,14 @@
 import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((450,500))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
